4fa6f302448900a774d08a1caee0079cd2088b55
Altiimax <44198671+Altiimax@users.noreply.github.com>
2019-04-09 14:39:54 +0200

    Jumper master base

--- js/basic.js
+++ js/basic.js
@@ -15,12 +15,18 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var map;
+var groundLayer;
 
 function preload ()
 {
-    this.load.image('background', 'img/AYDixaJ.jpg.png');
+    this.load.image('background', 'assets/img/AYDixaJ.jpg.png');
+    this.load.tilemapTiledJSON('map', 'assets/basiclayer.json');
 }
 
 function create () {
     this.add.image(400, 300, 'background');
+    map = this.make.tilemap({ key: 'map' });
+    map.createDynamicLayer('Background Layer', groundTiles, 0, 0);
+    groundLayer = map.createDynamicLayer('Ground Layer', groundTiles, 0, 0);
 }
